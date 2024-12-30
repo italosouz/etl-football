3f652ab62fdb550b6c10e176aba96c325a4ff0ed
Italo <italoaurilio@gmail.com>
2024-12-29 22:21:22 -0300

    add new functions

--- src/main.py
+++ src/main.py
@@ -1,28 +1,57 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-url = "https://fakestoreapi.com/users/"
+load_dotenv()
 
-response = requests.get(url)
-data = response.json()[0]
+def extract_temp_data(lat, lon, lang, units, apikey):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apikey}&lang={lang}&units={units}"
 
-for dado in data:
-    nome_completo = f"{
-        ['name']['firstname']} {data['name']['lastname']}"
-    email = data['email']
-    telefone = data['phone']
-    cidade = data["address"]["city"]
-    rua = data["address"]["street"]
-    numero = data["address"]["number"]
-    cep = data["address"]["zipcode"]
+    response = requests.get(url)
+    data = response.json()
+
+    return data
+
+def tranform_temp_data(data):
+    clima = data['weather'][0]['main']
+    clima_detail = data['weather'][0]['description']
+    temp = data['main']['temp']
+    sensacao_termica = data['main']['feels_like']
+    temp_min = data['main']['temp_min']
+    temp_max = data['main']['temp_max']
+    pressao_atm = data['main']['pressure']
+    umidade = data['main']['humidity']
+    visibilidade = data['visibility']
+    velocidade_vento = data['wind']['speed']
+    direcao_vento = data['wind']['deg']
+    cidade = data['name']
 
     dados_transformados = {
-        "nome_completo": nome_completo,
-        "email": email,
-        "telefone": telefone,
+        "clima": clima,
+        "clima_detail": clima_detail,
+        "temp": temp,
+        "sensacao_termica": sensacao_termica,
+        "temp_min": temp_min,
+        "temp_max": temp_max,
+        "pressao_atm": pressao_atm,
+        "umidade": umidade,
+        "visibilidade": visibilidade,
+        "velocidade_vento": velocidade_vento,
+        "direcao_vento": direcao_vento,
         "cidade": cidade,
-        "rua": rua,
-        "numero": numero,
-        "cep": cep
     }
 
-    print(dados_transformados)
+    return dados_transformados
+
+if __name__== "__main__":
+    APIKEY = os.getenv("APIKEY")
+
+    lat = -5.917550296148145
+    lon = -35.20936332816193
+    lang = "pt_br"
+    units = "metric"
+
+    data = extract_temp_data(lat, lon, lang, units, apikey=APIKEY)
+    new_data = tranform_temp_data(data)
+
+    print(new_data)
